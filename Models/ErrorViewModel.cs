896e063252cf5786dc59421c8d07773c5072a68d
laboratorio <laboratorio@UPOAULA10318>
2024-10-29 07:26:06 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace examen_p1_eg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
